3f308cf12274e6d3ecf1bed9b3dadca15327f23d
Stephen James <sjames@examsoft.com>
2022-04-17 07:33:07 -0500

    remove code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
 import "./styles/index.css";
 
-const p = (
-  attrs: any,
-  ...children: [HTMLElement | string]
-): HTMLParagraphElement => {
-  const p = document.createElement("p");
-  children.forEach((c) => {
-    if (typeof c === "string") {
-      p.textContent = c;
-    } else {
-      p.appendChild(c);
-    }
-  });
-  return p;
-};
-
 const app = (): HTMLElement => {
-  return p("", "this is a test");
+  const p = document.createElement("p");
+  p.textContent = "This is a test";
+  return p;
 };
 
 const main = () => {
